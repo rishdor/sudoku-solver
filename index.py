c9cefc6a2be970bedcc8c8d4bb4b6ae3672aed32
rina <doroshukmaryna@gmail.com>
2023-12-23 21:17:54 +0100

    recognizing main contour

--- index.py
+++ index.py
@@ -6,21 +6,24 @@ import numpy as np
 img = cv.imread('img\sudoku5.png')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-gray = gray.astype('uint8')
-
 blurred = cv.GaussianBlur(gray, (5, 5), 0)
 
-thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+thresh = cv.adaptiveThreshold(blurred, 255, 1, 1, 11, 2)
 
-resized = cv.resize(thresh, (200, 200), interpolation = cv.INTER_AREA)
-
-normalized = resized / 255.0
-
-cv.imshow('Preprocessed image', resized)
-cv.waitKey(0)
-cv.destroyAllWindows()
+resized = cv.resize(thresh, (300, 300), interpolation = cv.INTER_AREA)
 
 # contour detection
+
+contours, _ = cv.findContours(resized, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+contours = sorted(contours, key=cv.contourArea, reverse=True)
+sudoku_contour = contours[0]
+
+resized = cv.cvtColor(resized, cv.COLOR_GRAY2BGR)
+cv.polylines(resized, [sudoku_contour], True, (0,255,0), 2)
+cv.imshow('Resized with contour', resized)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
 # perspective transformation
 
