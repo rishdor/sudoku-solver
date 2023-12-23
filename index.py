63970489533362bb5dd6b9eb9b8ea40bb3cb2bb5
rina <doroshukmaryna@gmail.com>
2023-12-23 21:40:44 +0100

    perspective transformation

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # preprocess the image
 
-img = cv.imread('img\sudoku5.png')
+img = cv.imread('img\sudoku2.png')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 blurred = cv.GaussianBlur(gray, (5, 5), 0)
@@ -26,6 +26,32 @@ cv.waitKey(0)
 cv.destroyAllWindows()
 
 # perspective transformation
+
+epsilon = 0.01 * cv.arcLength(sudoku_contour, True)
+approx = cv.approxPolyDP(sudoku_contour, epsilon, True)
+
+corners = approx.ravel().reshape(-1, 2)
+corners = sorted(corners, key=lambda x: x[1])
+
+left = corners[:2]
+right = corners[2:]
+
+left = sorted(left, key=lambda x: x[0])
+
+right = sorted(right, key=lambda x: x[0], reverse=True)
+
+ordered_corners = np.float32(left + right)
+
+input_coords = np.float32(ordered_corners)
+output_coords = np.float32([[0,0], [299,0], [299,299], [0,299]])
+
+matrix = cv.getPerspectiveTransform(input_coords, output_coords)
+
+transformed_img = cv.warpPerspective(resized, matrix, (300,300))
+
+cv.imshow('Transformed Image', transformed_img)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
 # cell segmentation
 
